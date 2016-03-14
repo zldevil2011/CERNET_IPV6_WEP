0a5be7c793df2f70b74a391b86aa163dfaf750a4
zldevil2011 <929034478@qq.com>
2016-03-14 18:37:38 +0800

    index.html(but bug in IE&360)

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, patterns, include
+import views.index
 
 urlpatterns = [
+	url('^$', views.index.index, name="index"),
 #     # url('^welcome/$', views.welcome.welcome(), name="welcome"),
 ]
