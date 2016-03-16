6faabdd74aefb8806238f7eb6dd8ad09bc84b30b
zldevil2011 <929034478@qq.com>
2016-03-16 19:42:06 +0800

    fix footer bug because the IE do not support the new Tag

--- app/views/data.py
+++ app/views/data.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request, "data.html", {});
+
+def imageInfo(request):
+	return render(request, "imageInfo.html", {});
+
+
 # Create your views here.
