4470f30e52ee97910cf098ad45bf0abefe587750
zldevil2011 <929034478@qq.com>
2016-04-17 11:01:02 +0800

    productList

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, patterns, include
 import views.index
 import views.data
-
+import views.products
 urlpatterns = [
 	url('^$', views.index.index, name="index"),
 	url('^data$', views.data.index, name="data_index"),
 	url('^dataInfo/(\d+)/$', views.data.dataInfo, name="dataInfo"),
+	url('^products/$', views.products.index, name="products_index"),
+	url('^productInfo/(\d+)/$', views.products.productInfo, name="productInfo"),
 #     # url('^welcome/$', views.welcome.welcome(), name="welcome"),
 ]
