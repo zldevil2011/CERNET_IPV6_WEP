c9c6467c795c1299910ad8b7d1a21d9def8376a1
zldevil2011 <929034478@qq.com>
2016-04-18 18:52:49 +0800

    some change

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import sys
 from django.http import HttpResponse
+from django.views.decorators.cache import cache_page
 
+@cache_page(15*60)
 def index(request):
 	return render(request, "index.html", {});
 # Create your views here.
