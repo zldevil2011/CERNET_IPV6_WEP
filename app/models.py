bcdcef3c6662f5e0cd9acea115a327d6b42ae638
zldevil2011 <929034478@qq.com>
2016-05-04 15:12:34 +0800

    add urls

--- app/models.py
+++ app/models.py
@@ -4,11 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 #用户信息
-class User(models.Model):
+class AppUser(models.Model):
 	user_id = models.AutoField(primary_key = True)
 	username = models.CharField(max_length = 100)
 	password = models.CharField(max_length = 100)
-
+	sex = models.IntegerField(default = 0)
+	nickname = models.CharField(max_length = 30)
+	portrait = models.CharField(max_length = 200, null = True)
+	email = models.EmailField(null=True)
+	register_date = models.DateField(auto_now_add = True)
+	phone = models.CharField(max_length=20, null=True)
+	location = models.CharField(max_length = 100, null = True)
+	
 	def __unicode__(self):
 		return str(self.username)
 
@@ -18,7 +25,7 @@ class Admin(models.Model):
 	admin_id = models.AutoField(primary_key = True)
 	username = models.CharField(max_length = 100)
 	password = models.CharField(max_length = 100)
-	priority = models.IntegerField()
+	priority = models.IntegerField(default = 0)
 
 	def __unicode__(self):
 		return str(self.username)
