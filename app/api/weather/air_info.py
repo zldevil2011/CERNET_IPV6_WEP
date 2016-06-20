4d1abfa4beef739e6084e6c620f096512e5d16e7
zldevil2011 <929034478@qq.com>
2016-06-20 14:12:16 +0800

    update air_info

--- app/api/weather/air_info.py
+++ app/api/weather/air_info.py
@@ -24,7 +24,7 @@ class AirInfo(APIView):
     def get(self, request, format=None):
         location = request.GET.get('location', '北京')
         try:
-            item = Air.objects.filter(location=location).order_by('-time')[0]
+            item = Air.objects.filter(location=location).order_by('-date', '-time')[0]
         except Air.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = AirSerializer(item)
